44980042c1fc9e00233050112ca7950b3c9e61a1
Nicolai Minter <nicolaithorsten.minter@stud.tu-darmstadt.de>
2021-07-15 16:37:09 +0200

    add: remove node ad grpc
    also renamend list_of_nodes to dict_nodes, since it a dict and to be consistent with the load balancer

--- src/Rendezvous/RendezvousHashing.py
+++ src/Rendezvous/RendezvousHashing.py
@@ -10,19 +10,22 @@ import src.Rendezvous.RendezvousHashing_pb2_grpc as RH_pb2_grpc
 import src.Rendezvous.RendezvousNode_pb2 as RN_pb2
 import src.Rendezvous.RendezvousNode_pb2_grpc as RN_pb2_grpc
 
+import src.Rendezvous.type_pb2 as type_pb2
+
+from collections import defaultdict
 import grpc
 import socket
 import argparse
 from concurrent import futures
 
 # look how to import from AbstractRouterClass since it is our abstract class. Maybe like this: RendezvousHashing(AbstractRouterClass(RH_pb2_grpc.RendezvousHashingServicer))
-# DONE: inheriting from two classes at the same time could be done by separating them with a comma
+# DONE: inheriting from two classes at the same time could be done by separating them with a comma (nice one!)
 class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServicer):
     def __init__(self) -> None:
         channel = grpc.insecure_channel("172.17.0.2:50050")
         self.server_information_stub = SI_pb2_grpc.ServerInformationStub(channel)
         ## TODO: Locking this attribute to ensure sync
-        self._list_of_nodes = {}
+        self._dict_nodes = {}
         self.get_all_nodes()
 
     def get_all_nodes(self):
@@ -32,10 +35,11 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
         request = SI_pb2.GetAllRequest(type=SI_pb2.NODE)
         responses = self.server_information_stub.get_all_(request)
         for response in responses:
-            self._list_of_nodes[response.name] = response.ip_address
+            self._dict_nodes[response.name] = response.ip_address
 
-    # TODO: if we use multiple routers, we need to ensure that all of them have the same _list_of_nodes, currently its gets only updated in the init.
+    # TODO: if we use multiple routers, we need to ensure that all of them have the same _dict_nodes, currently its gets only updated in the init.
     # DONE: update the ServerInformation, since its updated, when a new node launches
+    # TODO: rethink done above. We maybe should just do it in the add_node
     def add_node(self, request, context):
         """
         adds a new Node into the Router.
@@ -43,8 +47,8 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
 
         node: the node that should be added
         """
-        print(self._list_of_nodes)
-        self._list_of_nodes[request.name] = request.ip_address
+        print(self._dict_nodes)
+        self._dict_nodes[request.name] = request.ip_address
         self.redistribute_objects_to_new_node(request.ip_address)
         return RH_pb2.RendezvousEmpty()
 
@@ -56,7 +60,7 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
         """
 
         # TODO: threading?
-        for _, n_ip in self._list_of_nodes.copy().items():
+        for _, n_ip in self._dict_nodes.copy().items():
             if n_ip == ip_address:
                 continue
 
@@ -64,10 +68,9 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
             channel = grpc.insecure_channel(n_ip)
             node_stub = RN_pb2_grpc.RendezvousNodeStub(channel)
             request = RN_pb2.NodeSendItemToNewNodeRequest(ip_address=ip_address)
-            # TODO: look into if this works
             node_stub.send_item_to_new_node(request)
 
-    # TODO: update the ServerInformation
+    # DONE: update the ServerInformation
     # TODO: remove_node not working
     def remove_node(self, request, context):
         """
@@ -76,7 +79,11 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
 
         node: the node that should be deleted
         """
-        del self._list_of_nodes[request.name]
+        request = SI_pb2.DeleteRequest(type=SI_pb2.NODE, name=request.name)
+        self.stub.delete_(request)
+
+        del self._dict_nodes[request.name]
+        
         self.redistribute_objects_from_deleted_node(request.ip_address)
 
     # TODO: it is not done, since it cant call the function on the node. Also the call was wrong
@@ -87,48 +94,44 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
 
         node: the node that should be deleted
         """
-        objects_on_node = {}
-        channel = grpc.insecure_channel(node[0])
+        objects_on_node = defaultdict(list)
+        channel = grpc.insecure_channel(ip_address)
         node_stub = RN_pb2_grpc.RendezvousNodeStub(channel)
         responses = node_stub.get_objects()
 
         # reconstruct the dictionary of objects saved on the node
-        for i, response in enumerate(responses):
-            objects_on_node[response.key] = response.value
+        for response in responses:
+            objects_on_node[response.key].append(response.value)
 
         # TODO: This solution is probably not efficient, but I'm not sure whether it's a design or programming issue. 
-        for k, v in objects_on_node.items():
-            self.forward_to_responsible_node(k, v)
+        for key, vs in objects_on_node.items():
+            champion_ip = self.find_responsible_node(key, self._dict_nodes.copy().items())
+            
+            channel = grpc.insecure_channel(champion_ip)
+            node_stub = RN_pb2_grpc.RendezvousNodeStub(channel)
+
+            for v in vs:
+                request = RN_pb2.NodeGetRequest(type=type_pb2.ADD,key=key,value=v)
+                responses = node_stub.get_request(request)
+
+                for _ in responses:
+                    pass
+
+        objects_on_node.clear()
 
 
     # DONE: the request gets forwarded directly from the router after finding the responsible node
     def forward_to_responsible_node(self, request, context):
         """
         finds the responsible node, for a given key
+        and sende the the request to the node.
 
         key: the key of which we want to know the node
 
         return the Node for the given key
         """
-        champion_ip = None
-        champion_name = None
-        maxValue = -1
 
-        # TODO: threading
-        for _, n_ip in self._list_of_nodes.copy().items():
-            # note: a node tuple consists of an ip_address and name
-            # connect to the node
-            channel = grpc.insecure_channel(n_ip)
-            node_stub = RN_pb2_grpc.RendezvousNodeStub(channel)
-
-            # calc the hash score from the node
-            request_node = RN_pb2.NodeHashValueForRequest(key=request.key)
-            currentValue = node_stub.hash_value_for_key(request_node)
-
-            # check if node is the biggest
-            if currentValue.hashValue > maxValue:
-                maxValue = currentValue.hashValue
-                champion_ip = n_ip
+        champion_ip = self.find_responsible_node(request.key, self._dict_nodes.copy().items())
 
         # creates a connection to the champion node
         channel = grpc.insecure_channel(champion_ip)
@@ -142,6 +145,28 @@ class RendezvousHashing(AbstractRouterClass, RH_pb2_grpc.RendezvousHashingServic
             pass
 
         return RH_pb2.RendezvousEmpty()
+
+    def find_responsible_node(self, key, dict_nodes_items):
+        champion_ip = None
+        maxValue = -1
+
+        # TODO: threading
+        for _, n_ip in dict_nodes_items:
+            # note: a node tuple consists of an ip_address and name
+            # connect to the node
+            channel = grpc.insecure_channel(n_ip)
+            node_stub = RN_pb2_grpc.RendezvousNodeStub(channel)
+
+            # calc the hash score from the node
+            request_node = RN_pb2.NodeHashValueForRequest(key=key)
+            currentValue = node_stub.hash_value_for_key(request_node)
+
+            # check if node is the biggest
+            if currentValue.hashValue > maxValue:
+                maxValue = currentValue.hashValue
+                champion_ip = n_ip
+        
+        return champion_ip 
 
 def serve(name, ip_address, port):
     # connects to the server information and registers itself
